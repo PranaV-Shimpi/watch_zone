1f227189e2bbb94c1f72b4801061caba255b9b28
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-06-18 19:15:36 +0530

    create: sidebar component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div className="mt-2">
+        <Sidebar />
+        {/* Video Render here */}
+      </div>
     </div>
   );
 };
