01deee07987323c69a565df74b686c596ffd33cd
PranaV Shimpi <23pranavshimpi@gmail.com>
2024-06-30 13:19:31 +0530

    Add: comments done

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { fetchComments } from '../functions/api';
+import React, { useEffect, useState } from "react";
+import { fetchComments } from "../functions/api";
 
 const CommentList = ({ videoId }) => {
   const [comments, setComments] = useState([]);
+  const [visibleComments, setVisibleComments] = useState(5); // Number of comments initially visible
 
   useEffect(() => {
     const fetchVideoComments = async () => {
       try {
-        const comments = await fetchComments(videoId);
-        setComments(comments);
+        const fetchedComments = await fetchComments(videoId);
+        setComments(fetchedComments);
       } catch (error) {
         console.error("Error fetching comments:", error);
       }
@@ -17,22 +18,49 @@ const CommentList = ({ videoId }) => {
     fetchVideoComments();
   }, [videoId]);
 
+  const toggleShowMore = () => {
+    setVisibleComments((prevVisibleComments) =>
+      prevVisibleComments === 5 ? comments.length : 5
+    );
+  };
+
   return (
-    <div className="mt-4">
+    <div className="mt-4 mb-10">
       {comments.length > 0 ? (
-        comments.map((comment) => (
-          <div key={comment.id} className="p-2 border-b border-gray-200">
-            <div className="flex items-center">
-              <img
-                src={comment.snippet.topLevelComment.snippet.authorProfileImageUrl}
-                alt={comment.snippet.topLevelComment.snippet.authorDisplayName}
-                className="rounded-full w-8 h-8 mr-2"
-              />
-              <span className="font-semibold">{comment.snippet.topLevelComment.snippet.authorDisplayName}</span>
+        <>
+          {comments.slice(0, visibleComments).map((comment) => (
+            <div key={comment.id} className="p-2 border-b border-gray-200">
+              <div className="flex items-center">
+                <img
+                  src={
+                    comment.snippet.topLevelComment.snippet
+                      .authorProfileImageUrl
+                  }
+                  alt={
+                    comment.snippet.topLevelComment.snippet.authorDisplayName
+                  }
+                  className="rounded-full w-8 h-8 mr-2"
+                />
+                <span className="font-semibold">
+                  {comment.snippet.topLevelComment.snippet.authorDisplayName}
+                </span>
+              </div>
+              <p className="ml-10">
+                {comment.snippet.topLevelComment.snippet.textDisplay}
+              </p>
             </div>
-            <p className="ml-10">{comment.snippet.topLevelComment.snippet.textDisplay}</p>
+          ))}
+          <div className="flex justify-center mt-2">
+            <button
+              className="text-blue-500 hover:underline"
+              onClick={toggleShowMore}
+            >
+              {visibleComments === 5
+                ? "Load more comments"
+                : "Show less Comments"}
+            </button>
           </div>
-        ))
+        </>
       ) : (
         <p>No comments yet.</p>
       )}
